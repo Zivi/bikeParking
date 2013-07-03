5cfb729b782a3d3ce83d18a8c5e2b81306d11fec
Zivi Weinstock <zivi.weinstock@gmail.com>
2013-07-03 10:28:48 -0700

    Data now parsing with Parse.

--- bikeParse.py
+++ bikeParse.py
@@ -1,15 +1,19 @@
-import json
-#pretty print
-from pprint import pprint
+import json,httplib
 
 json_data = open("bikeParkingShort.json")
-
-
 data = json.load(json_data)
-#returning error saying "value error: extra data: line 1 column7 - end of doc"
 
+connection = httplib.HTTPSConnection('api.parse.com', 443)
+connection.connect()
 
-
-#parse json.data for each element[i[18][19]]
-#18 & 19 are the lat/long coordinates for the bike racks
-#make new bikeParkingParsed json file with full data
+for bike_rack in data["data"]:
+  connection.request('POST', '/1/classes/bikeParking', json.dumps({
+                     "latitude": float(bike_rack[18]),
+                     "longitude": float(bike_rack[19])
+                  }), {
+  "X-Parse-Application-Id": "xSId1N6iguBFx6Neo3arIQ0M206c491XdMS8T2V5",
+  "X-Parse-REST-API-Key": "Xjv8hhYF1IsVcCnTggigPYowP7wc3KwW1Xdm18TZ",
+  "Content-Type": "application/json"
+  })
+  result = json.loads(connection.getresponse().read())
+  print result
